d5f7146b432e7f8db00cd381c1b013eb4bf82d92
Yadavendra Sakharkar <prajwal1619@gmail.com>
2024-12-15 22:29:03 +0530

    adding task and user models

--- models/task.go
+++ models/task.go
@@ -1,1 +1,14 @@
 package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+//creation of task schema
+
+type Task struct {
+	ID		primitive.ObjectID	`bson:"_id,omitempty" json:"id"`
+	Description		string	`bson:"description" json:"description"`
+	Completed		bool	`bson:"completed" json:"completed"`
+	Owner		primitive.ObjectID	`bson:"owner" json:"owner"`
+}
--- models/user.go
+++ models/user.go
@@ -1,1 +1,15 @@
 package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// creation of user schema
+
+type User struct {
+	ID		primitive.ObjectID	`bson:"_id,omitempty" json:"id"`
+	Name		string	`bson:"name" json:"name"`
+	Email		string	`bson:"email" json:"email"`
+	Password		string	`bson:"password" json:"password"`
+	Tokens   []string           `bson:"tokens,omitempty" json:"tokens,omitempty"`
+}

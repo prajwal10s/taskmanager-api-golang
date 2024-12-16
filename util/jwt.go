e3aecc2d442ebeeff8b64295459b49245d730908
Yadavendra Sakharkar <prajwal1619@gmail.com>
2024-12-16 13:10:42 +0530

    adding token functions

---
+++ util/jwt.go
@@ -0,0 +1,38 @@
+package utils
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+var jwtSecret = []byte("your_secret_key")
+
+type Claims struct {
+	UserID string `json:"user_id"`
+	jwt.StandardClaims
+}
+
+func GenerateJWT(userID string) (string, error) {
+	claims := &Claims{
+		UserID: userID,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(72 * time.Hour).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtSecret)
+}
+
+func VerifyJWT(tokenString string) (*Claims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return jwtSecret, nil
+	})
+
+	if claims, ok := token.Claims.(*Claims); ok && token.Valid {
+		return claims, nil
+	}
+	return nil, err
+}

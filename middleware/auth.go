cccecb94064e8e0bc79a4367f09728e74f90fb88
Yadavendra Sakharkar <prajwal1619@gmail.com>
2024-12-16 14:04:27 +0530

    adding auth

--- middleware/auth.go
+++ middleware/auth.go
@@ -1,1 +1,36 @@
 package middleware
+
+import (
+	"net/http"
+	"strings"
+
+	utils "taskmanager/util"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Extract Authorization header
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid or missing token"})
+			c.Abort()
+			return
+		}
+
+		token := strings.TrimPrefix(authHeader, "Bearer ")
+
+		// Validate JWT token
+		claims, err := utils.VerifyJWT(token)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			c.Abort()
+			return
+		}
+
+		// Set user info in the context
+		c.Set("user_id", claims.UserID)
+		c.Next()
+	}
+}
